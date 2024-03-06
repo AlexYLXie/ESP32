68a544052bed3e8794270e157813c07b7a0214a8
AlexYLXie <76693303+AlexYLXie@users.noreply.github.com>
2024-03-06 21:46:06 +0800

    Add files via upload

---
+++ ESP32_UART_Test.m
@@ -0,0 +1,45 @@
+clear 
+close all
+clc
+
+% Setting Uart COM port
+prompt = "Please key in the COM port of ESP32 device: ";
+COM = input(prompt,'s');
+s = serialport(['COM', COM],115200);
+
+% Basic setting of sensor
+s_rate = 500;
+t_window = 10;
+% t = 1/s_rate : 1/s_rate : 10;
+raw_data = [];
+plot_period = 0.1;  % period of plot waveform (sec)
+idx = 1;
+d = datetime('now','Format','y-MMM-d_HH-mm-ss');
+log_file_path = './temp_'+ string(d) + '.txt';
+
+fileID = fopen(log_file_path,'w');
+fprintf(fileID,'Time, Raw (V)\n');
+
+% Start collecting UART data
+figure(1);
+while 1
+    d = datetime('now','Format','y-MMM-d HH:mm:ss'); % Get current time
+    temp = char(read(s,7,"string"));
+    % raw_data = Data_shift(raw_data, shift);
+    raw_data(idx) = str2double(temp(2:5))/4096*3.3;
+    fprintf(fileID,'%s, %.3d\n',d, raw_data(idx));
+    idx = idx+1;
+
+    if mod(idx, s_rate*plot_period) == 0
+        t = 1/s_rate : 1/s_rate : length(raw_data)/s_rate;
+        plot(t, raw_data)
+        xlim([0, 10])
+        drawnow
+    end
+
+    if idx >= s_rate * t_window
+        idx = 1;
+        raw_data = [];
+    end
+end
+
